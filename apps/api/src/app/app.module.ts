fbeb1b02c7d35cc7fbbf45852008fb8e9085b8dc
Senosiain, Miguel <miguel.senosiain@mmc.com>
2026-02-13 11:03:31 -0300

    refactor: simplificar AppController sin AppService

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MembersModule } from './members/members.module';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+import {AppController} from './app.controller';
+import {MembersModule} from './members/members.module';
 
 @Module({
   imports: [
@@ -12,6 +11,5 @@ import { MembersModule } from './members/members.module';
     MembersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
